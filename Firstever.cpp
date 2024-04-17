9f0e81a54800871fb57f311f102dde0408e3d033
ChipuPhiri <167244308+ChipuPhiri@users.noreply.github.com>
2024-04-17 15:31:14 +0200

    Update Firstever.cpp

---
+++ Firstever.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+string yourName;
+cout << "enter name: ";
+cin >> yourName;
+cout << "hello " + yourName << endl;
+
+return 0;
+}
